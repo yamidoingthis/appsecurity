c1886d16b864effca11d751e493270473e915292
yamidoingthis <119778737+yamidoingthis@users.noreply.github.com>
2024-01-29 13:52:02 +0800

    Add project files.

---
+++ WebApplication3/Pages/Register.cshtml.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Pages
+{
+    public class RegisterModel : PageModel
+    {
+
+        private UserManager<IdentityUser> userManager { get; }
+        private SignInManager<IdentityUser> signInManager { get; }
+
+        [BindProperty]
+        public Register RModel { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
+
+        public void OnGet()
+        {
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = RModel.Email,
+                    Email = RModel.Email
+                };
+                var result = await userManager.CreateAsync(user, RModel.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return Page();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ WebApplication3/ViewModels/Register.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace WebApplication3.ViewModels
+{
+    public class Register
+    {
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        
+        public string CreditCardNo { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string MobileNo { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Upload)]
+        public IFormFile Photo { get; set; }
+
+        [Required]
+        public string AboutMe { get; set; }
+
+
+    }
+}
